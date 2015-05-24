8bc1dae44d14fcca8f17b0966459e32544344736
Ahmed Sa3d <ahmeds.elghareeb@gmail.com>
2015-05-24 20:17:50 +0200

    basic file management system for reading & writing to files.

---
+++ fms.py
@@ -0,0 +1,27 @@
+'''
+Name: File Management System.
+Details:
+## this file should implement A manager class for reading, writing and deleting to/from files.
+## Also it should convert the extracted information to the proper type.
+## the operations that should be implemented are CRUD "Create - Read - Update - Delete"
+Author: Zee93
+'''
+import json
+
+
+class Manager(object):
+
+    def create(name):
+        pass
+
+    def read(fname):
+        with open('data/' + fname + '.json', 'r+') as p_file:
+            projects = json.load(p_file)
+        return [project.get('name') for project in projects]
+
+    def update(name):
+        pass
+
+    def delete(name):
+        pass
+manager = Manager()
