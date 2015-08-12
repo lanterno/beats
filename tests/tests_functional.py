7932e3daf45dcf4577241cca7bf4216f7a77902c
Ahmed khan <ahmeds.elghareeb@gmail.com>
2015-08-12 12:56:30 +0200

    update .

---
+++ tests/tests_functional.py
@@ -0,0 +1,9 @@
+'''
+We will have one functional test to start with. It should cover the following
+
+1- workon a special directory for testing called testing inside data folder.
+2- create a couple of new projects
+3- start working on one of them
+4- wait a few know seconds then stop working on it.
+5- check the functionalities and the data.
+'''
