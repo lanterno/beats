388f1b2b56a195cebbb18af4f0e5f32d0118cc62
Sa3d <ahmeds.elghareeb@gmail.com>
2020-06-20 02:51:53 +0200

    Feat: Adds basic endpoints

--- service/server.py
+++ service/server.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 
+from service.models import ProjectManager
+
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/projects")
+async def list_projects():
+    return list(ProjectManager.list())
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/projects")
+async def create_project():
+    pass
+
+
+@app.post("/projects/{project_id}/start")
+async def start_project_timer(project_id: int):
+    pass
+
+
+@app.post("/projects/{project_id}/stop")
+async def end_project_timer(project_id: int):
+    pass
