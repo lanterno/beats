e74c6e35a1ea3a1f4ee14ed238d01c909c6a218f
Rancho <me@rancho.me>
2020-07-21 23:30:57 +0200

    chore: clean and remove old project traces

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Projects Time Control
-
-import sys
-from execute import execute_from_command_line
-
-
-if __name__ == "__main__":
-    execute_from_command_line(sys.argv[1:])
