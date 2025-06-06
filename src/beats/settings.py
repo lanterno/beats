9736698e7877f3f5f8b775e4894d67b7004c39c8
Ahmed Elghareeb <me@rancho.me>
2025-06-06 15:40:16 +0200

    style: uses ruff to clean up code

--- src/beats/settings.py
+++ src/beats/settings.py
@@ -1,9 +1,10 @@
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
-    db_dsn: str = Field(..., env='DB_DSN')
-    db_name: str = Field(default="ptc", env='DB_NAME')
+    db_dsn: str = Field(..., env="DB_DSN")
+    db_name: str = Field(default="ptc", env="DB_NAME")
     access_token: str = Field(default="secret", env="ACCESS_TOKEN")
 
 
